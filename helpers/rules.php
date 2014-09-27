9f8a56f8b9d937443f2778c7f3eaaba043c1fef9
Idan Mitrofanov (@idanm_) <idan1986@gmail.com>
2014-09-27 23:44:02 +0300

    RulesHelper added with full functionality but not tested one :D

---
+++ helpers/rules.php
@@ -0,0 +1,64 @@
+<?php
+
+class RulesHelper extends UtilsHelper {
+    public function phrase($param, $value, $visible) {
+        $condition = false;
+
+        switch($param) {
+            case 'all':
+                $condition = true;
+                break;
+            case 'post_type':
+                $condition = get_post_type() === $value;
+                break;
+            case 'post':
+                $condition = is_single($value);
+                break;
+            case 'category':
+                $condition = is_category($value) || (is_single() && in_category($value));
+                break;
+            case 'page':
+                $condition = is_page($value);
+                break;
+            case 'template':
+                $condition = is_page_template($value);
+                break;
+        }
+
+        return $condition && $visible;
+    }
+
+    public function priority($param) {
+        $data = array(
+            'all' => 1,
+            'post_type' => 2,
+            'category' => 3,
+            'template' => 4,
+            'post' => 5,
+            'page' => 5
+        );
+
+        return $data[$param];
+    }
+
+    public function SortById($a, $b) {
+        return strcmp($a['id'] ,$b['id']);
+    }
+
+    public function test($data) {
+        $results = array();
+
+        foreach($data as $item) {
+            $result = array(
+                'priority' => $this->priority($item['param']),
+                'phrase' => $this->phrase($item['param'], $item['value'], $item['visible'])
+            );
+
+            array_push($results, $result);
+        }
+
+        uasort($results, array($this, 'SortById'));
+
+        return $results[size($results) - 1]['phrase'];
+    }
+}
